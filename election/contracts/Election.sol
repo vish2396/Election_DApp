c682a6ae483b3035a836405048bfc680060b6606
vibhu <53181187+vish2396@users.noreply.github.com>
2023-04-07 12:46:11 +0530

    Master

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -1,12 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0;
 
-contract SLElection {
-    // Read/write candidate
-    string public candidate;
+contract Election {
+    //Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Read/write candidate
+    mapping(uint => Candidate) public candidates;
+    //Store Candidates Count
+    uint public candidatesCount;
+
+    //voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+    
     // Constructor
-    function Election () public {
-       candidate = "Candidate 1";
+    constructor(string memory _name1, string memory _name2) public {
+        addCandidate(_name1);
+        addCandidate(_name2);
+    }
+    
+    function addCandidate (string memory _name) public {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+     function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
