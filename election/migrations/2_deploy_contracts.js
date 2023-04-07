e8f8a4c3dc0d112eaa117f4d035bd0d4208878c4
vibhu <53181187+vish2396@users.noreply.github.com>
2023-04-07 12:46:44 +0530

    Master

--- election/migrations/2_deploy_contracts.js
+++ election/migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
-var Election = artifacts.require("Election.sol");
+/*var Election = artifacts.require("Election.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Election, { gas: 5000000 });
+};*/
+
+const Election = artifacts.require("Election");
+
+module.exports = function(deployer) {
+  deployer.deploy(Election, "Candidate 1", "Candidate 2");
 };
